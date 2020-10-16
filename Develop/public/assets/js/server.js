97b08b9543c74fe1f5713599521902ee2709e037
Caleb Chaney <calebc347@gmail.com>
2020-10-16 09:37:03 -0500

    Updated Javascript

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,26 +1,33 @@
 // Dependencies
-const express = require("express");
-const path = require("path");
-const uuidv4 = require("uuid/v4");
+let express = require("express");
+let path = require("path");
 
 
 // Sets up Express 
 let app = express();
-let id = uuidv4();
 let PORT = process.env.PORT || 3000;
 
+
+// Create a unique ID
+function createID(){
+    let time = new Date().getTime();
+    let uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        let r = (time + Math.random()*16)%16 | 0;
+        time = Math.floor(time/16);
+        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+}
+
+console.log(createID());
+
+// Sets up Express to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-
-// Create Server
-let server = app.createServer(handleRequest);
-function handleRequest(req, res) {
-    res.end("Server Created" + req.url);
-}
 
 
 // Server Listener 
 app.listen(PORT, function () {
     console.log("App Listening" + PORT)
 })
+
